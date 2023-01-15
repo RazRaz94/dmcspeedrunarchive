b6a8e3a8082876398d0acf1bd4d44fb89da5ea74
Raza <32294007+RazRaz94@users.noreply.github.com>
2023-01-16 01:35:10 +0300

    Made accordion needs to put it in table style. updated table colors

--- resources/views/pages/DMC1/DMC100Percent.blade.php
+++ resources/views/pages/DMC1/DMC100Percent.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 
 <link rel="stylesheet" href="{{asset('css/DMC1.css')}}">
-
-
 <link rel="stylesheet" href="{{asset('css/ToggleableTabs.css')}}">
+<link rel="stylesheet" href="{{asset('css/accordion.css')}}">
+<script src="{{asset('js/ToggleableTabs.js')}}"></script>
 
 <div id = "content">
     <div id = "left">
@@ -33,11 +33,36 @@
             {{"The objective is 100% game completion, meaning all significant interactions and criteria in each Missions needs to be performed and accomplished."}}
             <ul>
                 <li>{{"Clearing all 23 missions with S rank"}}</li>
-                <li>{{"Clearing all 12 secret missions"}}<sup>(1)</sup></li>
-                <li>{{"All permanent upgrades must be purchased and collected"}}<sup>(2)</sup></li>
+                <li>{{"Clearing all 12 secret missions "}}<sup>(1)</sup></li>
+                <li>{{"All permanent upgrades must be purchased and collected "}}<sup>(2)</sup></li>
                 <li>{{"All usable equipment must be collected "}}<sup>(3)</sup></li>
-
+            </ul>
+            <sup>(1) </sup>{{"All of them need to be cleared when you finish the game. The rewards you get from these missions are part of full health upgrade requirement.
+            Which is the Blue Orb Fragment"}}
         </dl>
+
+        <section class="accordion">
+          <input type="checkbox" name="collapse" id="handle1">
+          <p class="handle">
+            <label for="handle1">List of all Secret Missions to clear</label>
+          </p>
+          <div class="textShow">
+            <ul>
+              <li>Secret Mission 1: Critical Hit</li>
+              <li>Secret Mission 2: Phantom Baby</li>
+              <li>Secret Mission 3: Phantom Baby II </li>
+              <li>Secret Mission 4: The Three Beasts</li>
+              <li>Secret Mission 5: One-Eyed Evil</li>
+              <li>Secret Mission 6: Wandering Ghosts</li>
+              <li>Secret Mission 7: Water Cell</li>
+              <li>Secret Mission 8: Treasure of the Reaper</li>
+              <li>Secret Mission 9: Stairway of Tranquility</li>
+              <li>Secret Mission 10: Shadow of Darkness</li>
+              <li>Secret Mission 11: Blue Gem in the Sky</li>
+              <li>Secret Mission 12: Hidden Bangle</li>
+          </ul>
+          </div>
+        </section>
 
 <div class ="wrapper">
     <div class="buttonWrapper">
