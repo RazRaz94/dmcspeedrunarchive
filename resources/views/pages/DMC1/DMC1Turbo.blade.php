55c47c72dbb3d5f5aa169efa49721780c6a7bbf1
Raza <32294007+RazRaz94@users.noreply.github.com>
2023-01-28 17:22:47 +0300

    quick turbo page setup

---
+++ resources/views/pages/DMC1/DMC1Turbo.blade.php
@@ -0,0 +1,11 @@
+@extends('Layout')
+@section('title', 'PC Turbo Setup')
+@section('content')
+<link rel="stylesheet" href="{{asset('css/Utilities.css')}}">
+
+<div id = "content">
+    <div id = "left">
+        <h1>PC Turbo Setup</h1>
+
+    </div>
+</div>
