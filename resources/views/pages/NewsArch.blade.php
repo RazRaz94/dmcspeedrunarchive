d26b7a32c81a934197f5e9f75d406363a8ac4996
Raza <32294007+RazRaz94@users.noreply.github.com>
2023-04-27 21:20:46 +0300

    spelling mistake

--- resources/views/pages/NewsArch.blade.php
+++ resources/views/pages/NewsArch.blade.php
@@ -9,7 +9,7 @@
     <div id ="left">
         <h1>News Archive</h1>
         <dl class="CF2">
-        <b>At the moment this is work in progress page due to gathering the datas from our previus events. You can see all of our previous work by scrolling through
+        <b>At the moment this is work in progress page due to gathering the datas from our previous events. You can see all of our previous work by scrolling through
              the twitter timeline below.</b>
             </dl>
             <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/DmcSpeedruns?ref_src=twsrc%5Etfw">Tweets by DmcSpeedruns</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
