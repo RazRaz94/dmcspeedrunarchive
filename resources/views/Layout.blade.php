45bf2ec257839ff7e62e14cda333b25a8f62bdd8
Raza <32294007+RazRaz94@users.noreply.github.com>
2023-04-28 02:43:37 +0300

    fixed favicon issue of not showing on all pages

--- resources/views/Layout.blade.php
+++ resources/views/Layout.blade.php
@@ -5,6 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>@yield('title') - DMCSpeedruns</title>
+<link rel="shortcut icon" href="{{ asset('favicon.ico')}}" type="image/x-icon">
 <link rel="stylesheet" href="{{asset('css/sidenav.css')}}">
 <link rel="stylesheet" href="{{asset('css/footer.css')}}">
 <link rel="stylesheet" href="{{asset('css/body.css')}}">
