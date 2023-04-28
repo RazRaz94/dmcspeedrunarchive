e17674bb9b769dea882a423777b5b127b208bca2
Raza <32294007+RazRaz94@users.noreply.github.com>
2023-04-28 03:04:03 +0300

    Custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('Layout')
+@section('content')
+
+<link rel="stylesheet" href="{{asset('css/DMCIndexPage.css')}}">
+<link rel="stylesheet" href="{{asset('css/FAQs.css')}}">
+
+<div id = "content">
+    <div id ="left">
+        <h1></h1>
+        <dl class="CF2">
+        <b>"This page you want to access does not exist"</b>
+            </dl> 
+    </div>
+
+    <div id="clearit"></div>
+    </div>
+@endsection
